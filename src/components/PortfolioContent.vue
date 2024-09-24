f2eaf78c22d3b21249c9ddccf6770587f4586451
scrsund <137787953+scrsund@users.noreply.github.com>
2024-09-24 10:11:16 +0200

    state management and for loops within portfolio content

--- src/components/PortfolioContent.vue
+++ src/components/PortfolioContent.vue
@@ -1,53 +1,61 @@
 <template>
   <section class="portfolio-content">
-    <h1>My Projects</h1>
+    <div class="title">
+      <h1>My Projects</h1>
+    </div>
     <div class="project-wrapper">
-      <div class="project">
+      <div class="project" v-for="(project) in projects" :key="project.title">
         <div class="project-header">
-          <h2>Website V1</h2>
+          <h2>{{project.title}}</h2>
           <div class="icon-heading">
-            <i class="fa-brands fa-vuejs"></i>
-            <i class="fa-brands fa-node-js"></i>
-            <i class="fa-solid fa-database"></i>
-            <i class="fa-brands fa-git"></i>
+            <i v-for="icon in getProjectIcons(project)" :key="icon" :class="icon"></i>
           </div>
         </div>
-        <p>Website V1 was the first iteration of this website. I had a bigger emphasis on the design and functionality portion of it, but decided that a simplistic design would be better for a portfolio appeal, which led me to create V2. The main difference between the two websites is that I added a HoverCard component through SHAD and connected the contact form to a database using express.js and postgreSQL. </p>
-      </div>
-      <div class="project">
-        <h2>Recipe App</h2>
-        <p>The Recipe App was the first Vue.js project I did on my own. Through inspiration from a project in the Udemy Course I was taking, I wanted to challenge my understanding of Vue.js by creating something simple to understand but had more complexity. </p>
-      </div>
-      <div class="project">
-        <h2>Trading Journal (coming soon)</h2>
-      </div>
-      <div class="project">
-        <h2>Turtle Cross</h2>
+        <p>{{project.description}}</p>
+        <div class="link">
+          <a :href="project.demoLink" target="_blank" rel="noopener noreferrer">Live Demo</a>
+          <a :href="project.gitHubLink" target="_blank" rel="noopener noreferrer">GitHub</a>
+        </div>
       </div>
     </div>
   </section>
 </template>
 
+<script>
+import { mapState, mapGetters } from 'vuex';
+
+export default {
+  computed: {
+    ...mapState(["projects"]),
+    ...mapGetters(['getProjectIcons'])
+  }
+}
+</script>
+
 <style scoped>
 /*PROJECT DISPLAY*/
-
-i {
-  font-size: 1.3rem;
-  color: white;
-}
 
 .portfolio-content {
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  padding: 2rem;
+  background: linear-gradient(to bottom var(--clr-1), white);
+}
+
+.title {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 2rem;
+  background-color: var(--clr-1);
+  border: 2px solid var(--clr-6);
+  
 }
 
 .portfolio-content h1 {
   font-size: 1.8rem;
-  margin-bottom: 2rem;
-
 }
 
 .portfolio-content h2 {
@@ -58,15 +66,24 @@ i {
   display: grid;
   grid-template-columns: repeat(2, 1fr);
   gap: 2rem;
-  width: 100%;
+  width: 70%;
   max-width: 1200px;
 }
 
+i {
+  font-size: 1.3rem;
+  color: white;
+}
+
+
+
+
 .project {
-  background-color: var(--clr-5);
+  background-color: var(--clr-6);
   padding: 1.5rem;
   border-radius: 8px;
   box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+  border: 1px solid var(--clr-1);
 }
 
 
@@ -89,4 +106,25 @@ i {
   display: flex;
   gap: .5rem;
 }
+
+.link {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+}
+
+a {
+  text-decoration: none;
+  padding: .5rem 1rem;
+  color: white;
+  transition: color .2s ease-in;
+  border: 1px solid white;
+  border-radius: 1rem;
+}
+
+a:hover {
+  color: var(--clr-6);
+}
+
 </style>
--- src/index.js
+++ src/index.js
@@ -3,89 +3,114 @@ import { createStore } from "vuex";
 const store = createStore({
   state() {
     return {
-      entries: [
-        {
-          title: "Project 1",
-          icon: "fa-solid fa-code",
-          description: "A recipe management web application using Vue.js. <br><br>• Reusable components<br>• State management<br>• Component communication patterns<br>• Scoped CSS styles",
-          heading: "Recipe App",
-          customBorderClass: "border-bottom-right",
-        },
-        {
-          title: "Project 2",
-          icon: "fa-solid fa-code",
-          description: "An interactive game using Python's Turtle graphics module. <br><br>• Object-Oriented Programming<br>• Real-time event handling<br>• Game loop management<br>• Modular code design",
-          heading: "Turtle Cross",
-          customBorderClass: "border-left-right",
-        },
-        {
-          title: "Project 3",
-          icon: "fa-solid fa-code",
-          description: "<i>• Vuex<br>• Vue Router<br>• PostgreSQL<br>• Express.js",
-          heading: "Trading Journal<br><i>(coming soon)</i>",
-          customBorderClass: "border-bottom-left",
-        },
-        // {
-        //   title: "Self-Taught",
-        //   icon: "fa-solid fa-graduation-cap",
-        //   description:
-        //     "I taught myself how to do music, how to understand financial markets, and now, how to code. Learning new skills and challenging myself is my hobby.",
-        //   heading: "2007 - Present",
-        //   customBorderClass: "border-top-bottom",
-        // },
-        // {
-        //   title: "Me",
-        //   icon: null,
-        //   description: null,
-        //   customBorderClass: "border-none",
-        // },
-        // {
-        //   title: "Leader",
-        //   icon: "fa-solid fa-trophy",
-        //   description:
-        //     "Led as a music instructor, youth leader, and preschool assistant. I've worked with people of all ages-- from 2 years to 60 years old.",
-        //   heading: "2016 - 2022",
-        //   customBorderClass: "border-top-bottom",
-        // },
-        // {
-        //   title: "Aspiring Developer",
-        //   icon: "fa-solid fa-code",
-        //   description:
-        //     "Python, JavaScript/HTML/CSS and postgreSQL. Although I'm open to both front and backend development, I've been enjoying frontend development recently.",
-        //   heading: "2023 - Present",
-        //   customBorderClass: "border-top-right",
-        // },
-        // {
-        //   title: "Financial Analyst",
-        //   icon: "fa-solid fa-coins",
-        //   description:
-        //     "Trading Forex, Cryptocurrency, and NFTs through market sentiment and technical analysis. I've been able to multiply my portfolio in the last 3 years.",
-        //   heading: "2021 - Present",
-        //   customBorderClass: "border-left-right",
-        // },
-        // {
-        //   title: "Musician",
-        //   icon: "fa-solid fa-music",
-        //   description:
-        //     "Playing the drums, guitar, piano, bass, and writing songs. I've played for crowds of thousands and have experience as a studio musician.",
-        //   heading: "2008 - 2019",
-        //   customBorderClass: "border-top-left",
-        // },
-      ],
-      // iconList: [
-      //   "fa-solid fa-music",
-      //   "fa-solid fa-code",
-      //   "fa-solid fa-people-group",
-      //   "fa-solid fa-graduation-cap",
-      //   "fa-solid fa-trophy",
-      //   "fa-solid fa-handshake-angle",
-      //   "fa-solid fa-person-running",
-      //   "fa-solid fa-guitar",
-      //   "fa-solid fa-drum",
-      //   "fa-solid fa-dumbbell",
-      //   "fa-solid fa-ice-cream",
-      //   "fa-solid fa-dog",
-      //   "fa-solid fa-gamepad",
+      icons:{
+        html: 'fa-brands fa-html5',
+        css: 'fa-brands fa-css3-alt',
+        js: 'fa-brands fa-js',
+        vue: 'fa-brands fa-vuejs',
+        express: 'fa-brands fa-node-js',
+        python: 'fa-brands fa-python',
+        sql: 'fa-solid fa-database',
+        git: 'fa-brands fa-git',
+        figma: 'fa-brands fa-figma',
+      },
+      projects: [{
+        title: 'Website V1',
+        icons: ["vue", "express", "sql", "git"],
+        description: 'Website V1 was the first iteration of this website. I had a bigger emphasis on the design and functionality portion of it, but decided that a simplistic design would be better for a portfolio appeal, which led me to create V2. The main difference between the two websites is that I added a HoverCard component through SHAD and connected the contact form to a database using express.js and postgreSQL.',
+        demoLink: 'https://my-website-ten-jet-20.vercel.app/home',
+        gitHubLink: 'https://github.com/scrsund/scr-website',
+      },
+      {
+        title: 'Recipe App',
+        icons: ["html", "css", "vue"],
+        description: 'The Recipe App was the first Vue.js project I did on my own. Through inspiration from a project in the Udemy Course I was taking, I wanted to challenge my understanding of Vue.js by creating something simple to understand but had more complexity.',
+        demoLink: '',
+        gitHubLink: 'https://github.com/scrsund/recipe-app',
+      },
+      {
+        title: 'Trading Journal',
+        icons: ["vue", "express", "sql", "git", "figma"],
+        description: '',
+        demoLink: '',
+        gitHubLink: 'https://github.com/scrsund/trading-journal',
+      },
+      {
+        title: 'Turtle Cross',
+        icons: ["python"],
+        description: '',
+        demoLink: '',
+        gitHubLink: 'https://github.com/scrsund/turtle-cross',
+      }
+    ],
+      // entries: [
+      //   {
+      //     title: "Project 1",
+      //     icon: "fa-solid fa-code",
+      //     description: "A recipe management web application using Vue.js. <br><br>• Reusable components<br>• State management<br>• Component communication patterns<br>• Scoped CSS styles",
+      //     heading: "Recipe App",
+      //     customBorderClass: "border-bottom-right",
+      //   },
+      //   {
+      //     title: "Project 2",
+      //     icon: "fa-solid fa-code",
+      //     description: "An interactive game using Python's Turtle graphics module. <br><br>• Object-Oriented Programming<br>• Real-time event handling<br>• Game loop management<br>• Modular code design",
+      //     heading: "Turtle Cross",
+      //     customBorderClass: "border-left-right",
+      //   },
+      //   {
+      //     title: "Project 3",
+      //     icon: "fa-solid fa-code",
+      //     description: "<i>• Vuex<br>• Vue Router<br>• PostgreSQL<br>• Express.js",
+      //     heading: "Trading Journal<br><i>(coming soon)</i>",
+      //     customBorderClass: "border-bottom-left",
+      //   },
+      //   {
+      //     title: "Self-Taught",
+      //     icon: "fa-solid fa-graduation-cap",
+      //     description:
+      //       "I taught myself how to do music, how to understand financial markets, and now, how to code. Learning new skills and challenging myself is my hobby.",
+      //     heading: "2007 - Present",
+      //     customBorderClass: "border-top-bottom",
+      //   },
+      //   {
+      //     title: "Me",
+      //     icon: null,
+      //     description: null,
+      //     customBorderClass: "border-none",
+      //   },
+      //   {
+      //     title: "Leader",
+      //     icon: "fa-solid fa-trophy",
+      //     description:
+      //       "Led as a music instructor, youth leader, and preschool assistant. I've worked with people of all ages-- from 2 years to 60 years old.",
+      //     heading: "2016 - 2022",
+      //     customBorderClass: "border-top-bottom",
+      //   },
+      //   {
+      //     title: "Aspiring Developer",
+      //     icon: "fa-solid fa-code",
+      //     description:
+      //       "Python, JavaScript/HTML/CSS and postgreSQL. Although I'm open to both front and backend development, I've been enjoying frontend development recently.",
+      //     heading: "2023 - Present",
+      //     customBorderClass: "border-top-right",
+      //   },
+      //   {
+      //     title: "Financial Analyst",
+      //     icon: "fa-solid fa-coins",
+      //     description:
+      //       "Trading Forex, Cryptocurrency, and NFTs through market sentiment and technical analysis. I've been able to multiply my portfolio in the last 3 years.",
+      //     heading: "2021 - Present",
+      //     customBorderClass: "border-left-right",
+      //   },
+      //   {
+      //     title: "Musician",
+      //     icon: "fa-solid fa-music",
+      //     description:
+      //       "Playing the drums, guitar, piano, bass, and writing songs. I've played for crowds of thousands and have experience as a studio musician.",
+      //     heading: "2008 - 2019",
+      //     customBorderClass: "border-top-left",
+      //   },
       // ],
       // {
       //   title: "Growth",
@@ -159,6 +184,11 @@ const store = createStore({
       // },
     };
   },
+  getters: {
+    getProjectIcons: (state) => (project) => {
+      return project.icons.map(iconKey => state.icons[iconKey])
+    }
+  }
 });
 
 export default store;
