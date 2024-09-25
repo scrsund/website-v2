6fe27e5afa52e7e8d76c9f90c1abf23d06b86675
scrsund <137787953+scrsund@users.noreply.github.com>
2024-09-25 13:39:07 +0200

    adding fonts and colors

--- src/App.vue
+++ src/App.vue
@@ -23,6 +23,8 @@ export default {
 <style>
 @import url("https://fonts.googleapis.com/css2?family=Raleway:wght@400;500;600;700&display=swap");
 
+@import url('https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,100..700;1,100..700&display=swap');
+
 @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
 
 html,
@@ -31,7 +33,7 @@ body {
   padding: 0;
   width: 100%;
   height: 100%;
-  font-family: "Raleway", sans-serif;
+  font-family: "Roboto", sans-serif;
   background-color: var(--clr-1);
   color: whitesmoke;
 }
@@ -44,6 +46,16 @@ body {
   --clr-5: #637c54;
   --clr-6: #3c3b3f;
   --clr-7: #6a6666;
+  --clr-ntr-1: #d2b48c;
+  --clr-ntr-2: #FFFFF0;
+  --clr-c1: #ffdb58;
+  --clr-c2: #cc5500;
+  --clr-c3: #e17b34;
+  --clr-c4: #e97451;
+  --clr-a1: #98ff98;
+  --clr-a2: #e0ffff;
+  --clr-a3: #967bb6;
+  --clr-a4: #d8bfd8;
 }
 
 .transition-component {
